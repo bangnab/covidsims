dcc0acf99c10466cfba35416d783a80fecc37792
ambnic <nicola.ambrosetti@stralfors.se>
2021-08-07 11:13:23 +0200

    Minor refactoring

--- mesa/run-batch.py
+++ mesa/run-batch.py
@@ -1,6 +1,6 @@
 from model import *
 
-nb_batches = 100
+nb_batches = 20
 running_steps = 1000
 
 
@@ -10,7 +10,7 @@ def batch_run():
         model.step()
 
     gini = model.datacollector.get_model_vars_dataframe()
-    return gini["infected"][running_steps]
+    return gini
 
 
 results = []
@@ -18,5 +18,5 @@ for i in range(nb_batches):
     print(f"Running iteration {i}...")
     results.append(batch_run())
 
-nb_virus_defeated = sum(map(lambda x: x < 1, results))
+nb_virus_defeated = sum(map(lambda x: x.tail(1)["infected"].values[0] < 1, results))
 print(f"Percentage of times the virus was defeated: {100.0 * nb_virus_defeated / nb_batches}")
